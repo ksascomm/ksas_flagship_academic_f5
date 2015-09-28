4ac149896485d33993c456a1a1b4c585cd36d2a7
ksascomm <ksasweb@jhu.edu>
2015-09-28 12:13:39 -0400

    IE 9 fixes

--- header.php
+++ header.php
@@ -12,20 +12,22 @@
   <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon-57x57-precomposed.png" />
 
   <!-- CSS Files: All pages -->
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/stylesheets/app.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/stylesheets/app.min.css">
   <!-- CSS Files: Conditionals -->
-  
+  <!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 10]>
+	    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/app.ie.css">
+	<![endif]-->
+
   <!-- Modernizr and Jquery Script -->
   <?php wp_enqueue_script('jquery'); ?> 
   <script src="<?php echo get_template_directory_uri() ?>assets/js/vendor/modernizr-min.js"></script>
   <?php wp_head(); ?>
 
-  <!-- IE Fix for HTML5 Tags -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-  <![endif]-->
+
   <?php include_once("parts-analytics.php"); ?>
 </head>
 <?php $theme_option = flagship_sub_get_global_options(); $color_scheme = $theme_option['flagship_sub_color_scheme']; global $blog_id; $site_id = 'site-' . $blog_id; ?>
