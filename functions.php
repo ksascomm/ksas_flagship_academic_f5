fe93f0233362f284cf7a9e4f6eeea5eb91d084f1
ksascomm <ksasweb@jhu.edu>
2015-10-28 13:14:07 -0400

    disabled inline styles

--- functions.php
+++ functions.php
@@ -207,4 +207,26 @@ function my_sitemap_replacement ($content) {
 }
 add_filter('simple_sitemaps-generated_urlset', 'my_sitemap_replacement');
 
+/* ======================================================================
+ * Disable-Inline-Styles.php
+ * Removes inline styles and other coding junk added by the WYSIWYG editor.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * ====================================================================== */
+
+add_filter( 'the_content', 'clean_post_content' );
+function clean_post_content($content) {
+
+    // Remove inline styling
+    $content = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $content);
+
+    // Remove font tag
+    $content = preg_replace('/<font[^>]+>/', '', $content);
+
+    // Remove empty tags
+    $post_cleaners = array('<p></p>' => '', '<p> </p>' => '', '<p>&nbsp;</p>' => '', '<span></span>' => '', '<span> </span>' => '', '<span>&nbsp;</span>' => '', '<span>' => '', '</span>' => '', '<font>' => '', '</font>' => '');
+    $content = strtr($content, $post_cleaners);
+
+    return $content;
+}
+
 ?>
