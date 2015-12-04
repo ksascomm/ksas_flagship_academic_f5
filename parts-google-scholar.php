a71030f8d76e4db6e89ca2a37e8daea552db1d94
ksascomm <ksasweb@jhu.edu>
2015-12-04 15:49:37 -0500

    create cookie so Google Scholar results display

--- parts-google-scholar.php
+++ parts-google-scholar.php
@@ -1,11 +1,39 @@
 <?php require_once('assets/functions/simple_html_dom.php');
 $google_id = get_post_meta($post->ID, 'ecpt_google_id', true);
 $google = new simple_html_dom;
-$google_url = 'http://scholar.google.com/citations?sortby=pubdate&user=' . $google_id . '&pagesize=100';
-$older_pubs = 'http://scholar.google.com/citations?hl=en&user=' . $google_id . '&pagesize=100&sortby=pubdate&view_op=list_works&cstart=100';
-$google = file_get_html($google_url);
+$google_url = 'http://scholar.google.com/citations?user=' . $google_id . '&view_op=list_works&sortby=pubdate&pagesize=10';
+$older_pubs = 'http://scholar.google.com/citations?user=' . $google_id;
+
+// Use curl to access the remote server and store cookies so it can complete requests to Google Scholar without errors.
+$curl = curl_init($google_url);
+
+$config['useragent'] = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
+
+curl_setopt($curl, CURLOPT_USERAGENT, $config['useragent']);
+curl_setopt($curl, CURLOPT_REFERER, 'https://www.domain.com/');
+
+$cookie_dir = TEMPLATEPATH . "/assets/functions/cookies/";
+//$config['cookie_file'] = $cookie_dir . md5($_SERVER['REMOTE_ADDR']) . '.txt';
+$config['cookie_file'] = $cookie_dir . 'tmp/scholarcookietemp.txt';
+
+// Configure curl options to set up requests properly.
+curl_setopt($curl, CURLOPT_COOKIEFILE, $config['cookie_file']);
+curl_setopt($curl, CURLOPT_COOKIEJAR, $config['cookie_file']);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Get the HTML data.
+$data = curl_exec($curl);
+curl_close($curl);
+
+//$google = file_get_html($google_url);
+
+$google = str_get_html($data);
 
 //Updated by TG on 9/2/14. Google changed citation markup from tr.item & td#col-title to tr.gsc_a_tr & td.gsc_a_t/c/y
+
+echo '<p><strong>Displaying the 10 most recent publications. (Please refresh the page if no publications initially appear.)</p>'; 
+echo '<p>View the <a href="' . $older_pubs . '">Google Scholar Profile</a> for complete publications list.</strong></p>';
 
 foreach($google->find('tr.gsc_a_tr') as $article) {
     $item['title']  = $article->find('td.gsc_a_t a', 0)->plaintext;
@@ -14,9 +42,8 @@ foreach($google->find('tr.gsc_a_tr') as $article) {
     $item['year']   = $article->find('td.gsc_a_y', 0)->plaintext;
     
     ?>
-    <p class="pub"><b><a href="http://scholar.google.com<?php echo $item['link'];?>"><?php echo $item['title']; ?></a></b></p>
-    <h6 class="pub"><?php echo $item['year']; ?>, <?php echo $item['pub']; ?></h6>
+    <p class="pub"><strong><a href="http://scholar.google.com<?php echo $item['link'];?>"><?php echo $item['title']; ?></a></strong></p>
+    <p><?php echo $item['year']; ?>, <?php echo $item['pub']; ?></p>
     
     
     <?php } ?>
-<p align="right"><b><a href="<?php echo $older_pubs; ?>">View Older Publications</a></b></p>
