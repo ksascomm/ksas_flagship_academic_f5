00939b0633bbaf0e4ba6aa1729e75b4222e54d39
ksascomm <ksasweb@jhu.edu>
2016-03-14 11:42:06 -0400

    removed unnecessary livereload script

--- footer.php
+++ footer.php
@@ -57,9 +57,6 @@
   <?php //Call all the javascript
   		get_template_part('parts', 'script-initiators'); 
   		wp_footer(); ?>
-  		 <!--<script src="//localhost:35729/livereload.js"></script>-->
-<script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.1.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
+
 	</body>
 </html>
