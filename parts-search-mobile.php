ed789b553534d8a1026a9a2dc4039ae2795b032f
ksascomm <ksasweb@jhu.edu>
2016-05-26 11:00:49 -0400

    removed links from mobile search_bar. increased size of search box on mobile.

--- parts-search-mobile.php
+++ parts-search-mobile.php
@@ -1,6 +1,6 @@
 <div class="search-bar small-12 columns">
 	<div class="row">
-		<div class="small-9 columns">
+		<div class="small-12 columns">
 		<?php $theme_option = flagship_sub_get_global_options(); 
 				$collection_name = $theme_option['flagship_sub_search_collection'];
 		?>
@@ -12,18 +12,8 @@
 				<label for="search-mobile" class="screen-reader-text">
 					Search This Website
 				</label>
-				<input type="text" id="search-mobile" name="q" placeholder="Search this site" aria-label="search"/>
-					
+				<input type="text" id="search-mobile" name="q" placeholder="Search this site" aria-label="search"/>	
 			</form>
 		</div>
-			<?php wp_nav_menu( array( 
-				'theme_location' => 'search_bar', 
-				'menu_class' => '', 
-				'fallback_cb' => 'foundation_page_menu', 
-				'container' => 'div',
-				'container_id' => 'search_links', 
-				'container_class' => 'small-3 columns links inline',
-				'depth' => 1,
-				'items_wrap' => '%3$s', )); ?> 
 	</div>	
 </div>	<!-- End #search-bar	 -->
