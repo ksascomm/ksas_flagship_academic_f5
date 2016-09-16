6ab1b9578c8053887305ac6b55b99a4e5806ce0f
ksascomm <ksasweb@jhu.edu>
2016-09-16 09:26:31 -0400

    "Modernizr.touch" now "Modernizr.touchevents"

--- assets/js/vendor/app.js
+++ assets/js/vendor/app.js
@@ -12,7 +12,7 @@
   });
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
+  if (Modernizr.touchevents && !window.location.hash) {
     $(window).load(function () {
       setTimeout(function () {
         window.scrollTo(0, 1);
