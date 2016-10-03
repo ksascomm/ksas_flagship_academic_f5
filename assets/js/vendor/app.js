5b0657d6e6524965047c446b93ac025e0f4b767a
ksascomm <ksasweb@jhu.edu>
2016-10-03 11:55:23 -0400

    defer video script

--- assets/js/vendor/app.js
+++ assets/js/vendor/app.js
@@ -372,3 +372,13 @@ function getParameterByName(name)
         }
     };
 })(jQuery, this);
+
+
+/*Defer Video*/
+function init() {
+var vidDefer = document.getElementsByTagName('iframe');
+for (var i=0; i<vidDefer.length; i++) {
+if(vidDefer[i].getAttribute('data-src')) {
+vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+} } }
+window.onload = init;
