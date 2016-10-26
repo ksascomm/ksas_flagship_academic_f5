92d6381e61d12c945eb62da6df290e2665951437
ksascomm <ksasweb@jhu.edu>
2016-10-26 14:56:11 -0400

    corrected file path

--- template-full-width.php
+++ template-full-width.php
@@ -6,7 +6,7 @@ Template Name: Full Width - No Sidebar
 <?php get_header(); ?>
 <div class="row wrapper radius10" id="page" role="main">
 	<div class="large-12 columns">	
-		<?php locate_template('parts-nav-breadcrumbs.php', true, false); ?>	
+		<?php locate_template('parts/nav-breadcrumbs.php', true, false); ?>	
 		<div class="content">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<h1 class="page-title"><?php the_title();?></h1>
