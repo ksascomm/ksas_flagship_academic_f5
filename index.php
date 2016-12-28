418be5be36ca6a4ae00f118017e04f1d54e2738f
ksascomm <ksasweb@jhu.edu>
2016-12-28 11:56:09 -0500

    blog archive (index.php) full-width

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
-<div class="row sidebar_bg radius10" id="page">
-	<div class="small-12 large-8 columns wrapper radius-left offset-topgutter">	
+<div class="row wrapper radius10" id="page" role="main">
+	<div class="small-12 columns">
 		<?php locate_template('parts/nav-breadcrumbs.php', true, false);
 		$theme_option = flagship_sub_get_global_options(); $news_query_cond = $theme_option['flagship_sub_news_query_cond']; ?>	
 		<main class="content post-archive" itemprop="mainEntity" itemscope itemtype="http://schema.org/Blog">
 		<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
+		<div class="small-12 large-11 columns">
 		<?php 
 			$paged = (get_query_var('paged')) ? (int) get_query_var('paged') : 1;
 			if ( false === ( $news_archive_query = get_transient( 'news_archive_query_' . $paged ) ) ) {
@@ -32,7 +33,7 @@
 			} 	
 
 		while ($news_archive_query->have_posts()) : $news_archive_query->the_post(); ?>
-
+	
 		<article role="article" aria-labelledby="post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
 			<link itemprop="mainEntityOfPage" href="<?php the_permalink(); ?>" />
 				<h3 class="uppercase black" itemprop="datePublished"><?php the_time( get_option( 'date_format' ) ); ?></h3>
@@ -70,12 +71,11 @@
 			</div>	
 		</article>		
 			<?php endwhile; ?>
-		
+		</div>	
 		<div class="row">
 			<?php flagship_pagination($news_archive_query->max_num_pages); ?>		
 		</div>	
 		</main>
 	</div>	<!-- End main content (left) section -->
-<?php locate_template('parts/sidebar.php', true, false); ?>
 </div> <!-- End #landing -->
 <?php get_footer(); ?>
