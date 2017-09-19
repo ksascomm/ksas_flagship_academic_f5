250d845c8ec647a918baa247ff4eb11a173e5ef2
ksascomm <ksasweb@jhu.edu>
2017-09-19 15:42:12 -0400

    remove unused pagination function; disable formidable dropzone

--- functions.php
+++ functions.php
@@ -214,72 +214,14 @@ function my_css_attributes_filter($var) {
   return is_array($var) ? array() : '';
 }
 
-
-/**********CUSTOM TEMPLATE PAGINATION******************/
-
-function custom_pagination($numpages = '', $pagerange = '', $paged='') {
-
-  if (empty($pagerange)) {
-    $pagerange = 2;
-  }
-
-  /**
-   * This first part of our function is a fallback
-   * for custom pagination inside a regular loop that
-   * uses the global $paged and global $wp_query variables.
-   * 
-   * It's good because we can now override default pagination
-   * in our theme, and use this function in default quries
-   * and custom queries.
-   */
-  global $paged;
-  if (empty($paged)) {
-    $paged = 1;
-  }
-  if ($numpages == '') {
-    global $wp_query;
-    $numpages = $wp_query->max_num_pages;
-    if(!$numpages) {
-        $numpages = 1;
-    }
-  }
-
-  /** 
-   * We construct the pagination arguments to enter into our paginate_links
-   * function. 
-   */
-  $pagination_args = array(
-    'base'            => get_pagenum_link(1) . '%_%',
-    'format'          => 'page/%#%',
-    'total'           => $numpages,
-    'current'         => $paged,
-    'show_all'        => False,
-    'end_size'        => 1,
-    'mid_size'        => $pagerange,
-    'prev_next'       => True,
-    'prev_text'       => __('&laquo;'),
-    'next_text'       => __('&raquo;'),
-    'type'            => 'plain',
-    'add_args'        => false,
-    'add_fragment'    => ''
-  );
-
-  $paginate_links = paginate_links($pagination_args);
-
-  if ($paginate_links) {
-    echo "<nav class='custom-pagination'>";
-      echo "<span class='page-numbers page-num'>Page " . $paged . " of " . $numpages . "</span> ";
-      echo $paginate_links;
-    echo "</nav>";
-  }
-
-}
-
 /**********INCREASE POST META OPTIONS******************/
 add_filter( 'postmeta_form_limit', 'meta_limit_increase' );
 function meta_limit_increase( $limit ) {
     return 100;
 }
+
+/*****FORMIDABLE UPLOADS ***/
+add_filter( 'frm_load_dropzone', '__return_false' );
 
 // Register scripts and stylesheets
 require_once(get_template_directory().'/assets/functions/enqueue-scripts.php'); 
