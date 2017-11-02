46513926833d46eb13b03a720e68998f8c2c5df4
ksascomm <ksasweb@jhu.edu>
2017-11-02 11:19:14 -0400

    update offcanvas and footer links

--- parts/offcanvas.php
+++ parts/offcanvas.php
@@ -12,41 +12,24 @@
                     <li role="menuitem"><a href="http://krieger.jhu.edu/people/faculty-directory/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Faculty')">Faculty Directory</a></li>
                     <li role="menuitem"><a href="http://krieger.jhu.edu/academics/fields/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Fields of Study')">Fields of Study</a></li>
                     <li role="menuitem"><a href="http://www.library.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Libraries')">Libraries</a></li>
-                    <li role="menuitem"><a href="http://krieger.jhu.edu/academics/majors-minors/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Majors & Minors')">Majors & Minors</a>
-                    <li role="menuitem"><a href="#" class="off-canvas-submenu-call">Other Educational Opportunities<span>+</span></a></li>
-                      <ul class="no-bullet off-canvas-submenu"> 
-                        <li role="menuitem"><a href="http://advanced.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'AAP')">Advanced Academic Programs</a></li>
-                        <li role="menuitem"><a href="http://web.jhu.edu/study_abroad" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Study Abroad')">Study Abroad</a></li>
-                        <li role="menuitem"><a href="http://pages.jh.edu/summer/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Summer')">Summer Programs</a></li>
-                      </ul>
+                    <li role="menuitem"><a href="http://krieger.jhu.edu/academics/majors-minors/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Majors & Minors')">Majors & Minors</a></li>
                   </ul>
               </li>
               <li class="black"><h3>Student & Faculty Resources</h3>    
                   <ul class="no-bullet" role="menu">
-                    <li role="menuitem"><a href="https://sis.jhu.edu/sswf/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'ISIS')">Course Listings & Registration</a></li>
-                    <li role="menuitem"><a href="https://www.jhu.edu/admissions/financial-aid/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Financial Aid')">Financial Aid</a></li>
-                    <li role="menuitem"><a href="https://hrnt.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Human Resources')">Human Resources</a></li>
-                    <li role="menuitem"><a href="#" class="off-canvas-submenu-call">KSAS Policies<span>+</span></a></li>
-                      <ul class="no-bullet off-canvas-submenu"> 
-                        <li role="menuitem"><a href="http://web.jhu.edu/Homewood-IRB" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'IRB')">Institutional Review Board</a></li>
-                        <li role="menuitem"><a href="http://krieger.jhu.edu/kasper" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'KASPER')">KASPER</a></li>
-                        <li role="menuitem"><a href="http://krieger.jhu.edu/research/policies/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Polices/Procedures')">Policies & Procedures</a></li>
-                      </ul>
-                    <li role="menuitem"><a href="http://web.jhu.edu/registrar" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">Registrar's Office</a></li>
-                    <li role="menuitem"><a href="#" class="off-canvas-submenu-call">Student Services<span >+</span></a></li>      
-                      <ul class="no-bullet off-canvas-submenu"> 
-                        <li role="menuitem"><a href="http://homewoodgrad.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Grad/Postdoc Affairs')">Graduate and Postdoctoral Affairs</a></li>
-                        <li role="menuitem"><a href="http://studentaffairs.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'HSA')">Homewood Student Affairs</a></li>
-                      </ul>
+          					<li role="menuitem"><a href="https://sis.jhu.edu/sswf/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'ISIS')">Course Listings & Registration</a></li>
+          					<li role="menuitem"><a href="https://www.jhu.edu/admissions/financial-aid/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Financial Aid')">Financial Aid</a></li>
+          					<li role="menuitem"><a href="https://hrnt.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Human Resources')">Human Resources</a></li>
+          					<li role="menuitem"><a href="https://studentaffairs.jhu.edu/registrar/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">Registrar's Office</a></li>
                   </ul>
               </li>
               <li class="black"><h3>Across Campus</h3>
                 <ul class="no-bullet" role="menu">
-                  <li role="menuitem"><a href="https://www.jhu.edu/admissions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Admissions')">Admissions Information</a></li>
-                  <li role="menuitem"><a href="https://www.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'JHU Home')">Johns Hopkins University Website</a></li>
-                  <li role="menuitem"><a href="https://www.jhu.edu/maps-directions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Maps/Directions')">Maps & Directions</a></li>
-                  <li role="menuitem"><a href="https://my.jh.edu/portal/web/jhupub" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'myJHU')">myJHU</a></li>
-                  <li role="menuitem"><a href="http://hub.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'TheHub')">The Hub</a> | <a href="http://hub.jhu.edu/at-work" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'HubAtWork')">at Work</a></li>
+        					<li role="menuitem"><a href="https://www.jhu.edu/admissions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Admissions')">Admissions Information</a></li>
+        					<li role="menuitem"><a href="https://www.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'JHU Home')">Johns Hopkins University Website</a></li>
+        					<li role="menuitem"><a href="https://www.jhu.edu/maps-directions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Maps/Directions')">Maps & Directions</a></li>
+        					<li role="menuitem"><a href="https://my.jh.edu/portal/web/jhupub" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'myJHU')">myJHU</a></li>
+        					<li role="menuitem"><a href="http://hub.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'TheHub')">The Hub</a></li>
                 </ul>
             </li>
             <li role="menuitem">
