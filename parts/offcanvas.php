2b2f37d49b3080df3e4b8f208c7161286b07dd33
ksascomm <ksasweb@jhu.edu>
2018-12-04 09:25:11 -0500

    Replace GSA with WP search

--- parts/offcanvas.php
+++ parts/offcanvas.php
@@ -1,48 +1,61 @@
- <?php $theme_option = flagship_sub_get_global_options(); 
-        $collection_name = $theme_option['flagship_sub_search_collection'];
-    ?>
+<?php $theme_option = flagship_sub_get_global_options();
+$collection_name = $theme_option['flagship_sub_search_collection'];
+?>
 <div class="off-canvas-bg hide-for-print">
   <div id="pattern" class="pattern">
     <div class="offcanvas-top">
-        <div class="o-content" aria-hidden="false" role="navigation" aria-label="Global Menu">
-          <ul class="small-block-grid-4" id="global-links">
-              <li class="black"><h3>Academics</h3>
-                  <ul class="no-bullet" role="menu">
-                    <li role="menuitem"><a href="https://krieger.jhu.edu/academics/departments-programs-and-centers/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Departments')";>Departments, Programs, and Centers</a></li>
-                    <li role="menuitem"><a href="https://krieger.jhu.edu/people/faculty-directory/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Faculty')">Faculty Directory</a></li>
-                    <li role="menuitem"><a href="https://krieger.jhu.edu/academics/fields/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Fields of Study')">Fields of Study</a></li>
-                    <li role="menuitem"><a href="https://www.library.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Libraries')">Libraries</a></li>
-                    <li role="menuitem"><a href="https://krieger.jhu.edu/academics/majors-minors/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Majors & Minors')">Majors & Minors</a></li>
-                  </ul>
-              </li>
-              <li class="black"><h3>Student & Faculty Resources</h3>    
-                  <ul class="no-bullet" role="menu">
-          					<li role="menuitem"><a href="https://sis.jhu.edu/sswf/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'ISIS')">Course Listings & Registration</a></li>
-          					<li role="menuitem"><a href="https://www.jhu.edu/admissions/financial-aid/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Financial Aid')">Financial Aid</a></li>
-          					<li role="menuitem"><a href="https://hrnt.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Human Resources')">Human Resources</a></li>
-          					<li role="menuitem"><a href="https://studentaffairs.jhu.edu/registrar/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">Registrar's Office</a></li>
-                  </ul>
-              </li>
-              <li class="black"><h3>Across Campus</h3>
-                <ul class="no-bullet" role="menu">
-        					<li role="menuitem"><a href="https://www.jhu.edu/admissions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Admissions')">Admissions Information</a></li>
-        					<li role="menuitem"><a href="https://www.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'JHU Home')">Johns Hopkins University Website</a></li>
-        					<li role="menuitem"><a href="https://www.jhu.edu/maps-directions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Maps/Directions')">Maps & Directions</a></li>
-        					<li role="menuitem"><a href="https://my.jh.edu/portal/web/jhupub" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'myJHU')">myJHU</a></li>
-        					<li role="menuitem"><a href="https://hub.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'TheHub')">The Hub</a></li>
-                </ul>
-            </li>
-            <li role="menuitem">
-                <a href="https://krieger.jhu.edu/giving/" class="button" onclick="ga('send', 'event', 'Offcanvas', 'ETC', 'Giving')"><span class="fa fa-gift"></span> Give Now!</a>
-                <form method="GET" action="<?php echo site_url('/search'); ?>" id="search-bar">
-                      <input type="submit" class="icon-search" aria-label="submit" value="&#xe004;" />
-                      <label for="search" class="screen-reader-text">Search</label>
-                      <input type="text" id="search" name="q" placeholder="Search this site" aria-label="search"/>
-                      <input type="hidden" aria-label="site" name="site" value="<?php echo $collection_name; ?>" />
-                </form>
-            </li>
-          </ul>
-          <hr class="hide-for-medium-up">
+      <div class="o-content" aria-hidden="false" role="navigation" aria-label="Global Menu">
+        <ul class="small-block-grid-4" id="global-links">
+          <li class="black"><h3>Academics</h3>
+            <ul class="no-bullet" role="menu">
+              <li role="menuitem"><a href="https://krieger.jhu.edu/academics/departments-programs-and-centers/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Departments')";>Departments, Programs, and Centers</a></li>
+              <li role="menuitem"><a href="https://krieger.jhu.edu/people/faculty-directory/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Faculty')">Faculty Directory</a></li>
+              <li role="menuitem"><a href="https://krieger.jhu.edu/academics/fields/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Fields of Study')">Fields of Study</a></li>
+              <li role="menuitem"><a href="https://www.library.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Libraries')">Libraries</a></li>
+              <li role="menuitem"><a href="https://krieger.jhu.edu/academics/majors-minors/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Majors & Minors')">Majors & Minors</a></li>
+            </ul>
+          </li>
+          <li class="black"><h3>Student & Faculty Resources</h3>
+            <ul class="no-bullet" role="menu">
+              <li role="menuitem"><a href="https://sis.jhu.edu/sswf/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'ISIS')">Course Listings & Registration</a></li>
+              <li role="menuitem"><a href="https://www.jhu.edu/admissions/financial-aid/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Financial Aid')">Financial Aid</a></li>
+              <li role="menuitem"><a href="https://hrnt.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Human Resources')">Human Resources</a></li>
+              <li role="menuitem"><a href="https://studentaffairs.jhu.edu/registrar/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">Registrar's Office</a></li>
+            </ul>
+          </li>
+          <li class="black"><h3>Across Campus</h3>
+            <ul class="no-bullet" role="menu">
+              <li role="menuitem"><a href="https://www.jhu.edu/admissions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Admissions')">Admissions Information</a></li>
+              <li role="menuitem"><a href="https://www.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'JHU Home')">Johns Hopkins University Website</a></li>
+              <li role="menuitem"><a href="https://www.jhu.edu/maps-directions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Maps/Directions')">Maps & Directions</a></li>
+              <li role="menuitem"><a href="https://my.jh.edu/portal/web/jhupub" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'myJHU')">myJHU</a></li>
+              <li role="menuitem"><a href="https://hub.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'TheHub')">The Hub</a></li>
+            </ul>
+          </li>
+          <li role="menuitem">
+            <a href="https://krieger.jhu.edu/giving/" class="button" onclick="ga('send', 'event', 'Offcanvas', 'ETC', 'Giving')"><span class="fa fa-gift"></span> Give Now!</a>
+            <form method="GET" action="<?php echo home_url( '/' ); ?>" role="search" aria-label="Utility Bar Search">
+              <div class="row">
+                <div class="large-7 columns">
+                  <div class="row collapse">
+                    <div class="small-2 columns">
+                      <button class="button postfix">
+                      <span class="fa fa-search" aria-hidden="true"></span>
+                      </button>
+                    </div>
+                    <div class="small-10 columns">
+                      <label for="s" class="screen-reader-text">
+                        Search This Website
+                      </label>
+                      <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search this site" aria-label="Search This Website"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </li>
+        </ul>
+        <hr class="hide-for-medium-up">
       </div>
     </div>
   </div>
--- search.php
+++ search.php
@@ -4,142 +4,68 @@ Template Name: Search Results
 */
 ?>
 <?php
-require_once TEMPLATEPATH . "/assets/functions/GoogleSearch.php";
-get_header(); 
-$theme_option = flagship_sub_get_global_options(); 
-$collection_name = $theme_option['flagship_sub_search_collection'];
+get_header();
 ?>
-
 <div class="row wrapper radius10">
     <main class="large-12 columns content" id="page">
-        <h1 class="page-title">Search Results</h1>
-        <?php 
-            try {
-                $search = new KSAS_GoogleSearch();
-                $resultsPageNum = 1;
-                if (array_key_exists('resultsPageNum', $_REQUEST)) {
-                    $resultsPageNum = $_REQUEST['resultsPageNum'];
-                }
-                $resultsPerPage = 35;
-                $baseQueryURL = 'https://search.johnshopkins.edu/search?&client=ksas_frontend';
-                $results = $search->query($_REQUEST['q'], $_REQUEST['site'], $baseQueryURL, $resultsPageNum, $resultsPerPage);
-                $hits = $results->getNumHits();
-                $displayQuery = $results->getDisplayQuery();
-                $docTitle = 'Search Results';
-                $sponsored_result = $results->getSponsoredResult();
-        ?>
-
-        <?php if ($hits > 0) { ?>
-           <form class="search-form" action="<?php echo site_url('/search'); ?>" method="get">
-                <fieldset> 
-                    <label>
-                        Search:
-                        <input type="text" class="input-text" name="q" value="<?php echo $displayQuery ?>" />
-                    </label>
-                    <div class="row">
-                        <div class="small-12 columns">
-                            <input type="radio" name="site" id="thisSite" value="<?php echo $collection_name; ?>">
-                                <label for="thisSite">This & Related Websites</label>
-                            <input type="radio" name="site" id="krieger" value="krieger_collection">
-                                <label for="krieger">Krieger Network</label>
-                            <input type="radio" name="site" id="allJHU" value="jhuedu">
-                                <label for="allJHU">All of JHU</label>
-                        </div>   
-                    </div>                    
-                    <input type="submit" class="button blue_bg" id="search_again" value="Search Again" />
-                        <label for="search_again" class="screen-reader-text">
-                        Search Again
-                        </label>
-                </fieldset>
-            </form> 
-
-        <h5 class="black">Results <strong><?php echo $results->getFirstResultNum() ?> - <?php echo $results->getLastResultNum() ?></strong> of about <strong><?php echo $hits ?></strong></h5>
-           
-        <?php if (empty($sponsored_result) == false) { ?>
-            <div class="panel radius10" id="sponsored">
-                <h2 class="black">Featured Result</h2>
-                <h3><a href="<?php echo $sponsored_result['sponsored_url']; ?>"><?php echo $sponsored_result['sponsored_title']; ?></a><small class="italic"> &mdash;<?php echo $sponsored_result['sponsored_url']; ?></small></h3>
-            </div>
-         <?php } ?>   
-            <div id="search-results">
-                <ul>
-                    <?php while ($result = $results->getNextResult()) {
-                        // note what results are PDFs
-                        $pdfNote = '';
-                        if (preg_match('{application/pdf}', $result['mimeType'])) {
-                            $pdfNote = '<span class="black"><span class="fa fa-file-pdf-o" aria-hidden="true"></span> [PDF]</span> ';
-                        }
-                        ?>
-                    <li>
-                        <h4><?php echo $pdfNote ?><a href="<?php echo $result['path'] ?>"><?php echo $result['title'] ?></a></h4>
-                        <?php if (array_key_exists('description', $result) && $result['description']) { ?>
-                             <p itemprop="articleBody"><?php echo $result['description'] ?></p>
-                        <?php } ?>
-                        <div class="url"><?php echo $result['path'] ?> - <?php echo $result['sizeHumanReadable'] ?></div>
-                    </li>
-                    <hr>
-                    <?php } ?>
-                </ul>
-            </div>
-             
-            <div class="section">
-                <?php
-                    $notices = $results->getNotices();
-                    foreach ($notices as $notice) {
-                        ?>
-                        <p class="notice"><?php echo $notice ?></p>
-                        <?php
-                    }
-                ?>
-                <div class="pagination">
-                    <?php foreach ($results->getResultsetLinks() as $resultsetLink) {
-                        print "$resultsetLink ";
-                    } ?>
-                    <?php echo $results->getNextLink() ?> 
-                </div>   
-            </div>
-        <?php } else {
-        // no hits
-        ?>
-             
-        <?php $notices = $results->getNotices(); foreach ($notices as $notice) { ?>
-            <p class="notice"><?php echo $notice ?></p>
-        <?php } ?>
-             
-            <p>Sorry, No Results. Try your search again on <a href="https://www.jhu.edu/search/">JHU.edu</a>, or enter a new search term below:</p>
-               <form class="search-form" action="<?php echo site_url('/search'); ?>" method="get">
-                    <fieldset> 
-                        <label>
-                            Search:
-                            <input type="text" class="input-text" name="q" value="<?php echo $displayQuery; ?>" />
-                        </label>
-                        <div class="row">
-                            <div class="small-12  columns">
-                                <input type="radio" name="site" id="thisSite" value="<?php echo $collection_name; ?>">
-                                    <label for="thisSite">This & Related Websites</label>
-                                <input type="radio" name="site" id="krieger" value="krieger_collection">
-                                    <label for="krieger">Krieger Network</label> 
-                                <input type="radio" name="site" id="allJHU" value="jhuedu">
-                                    <label for="allJHU">All of JHU</label>
-                            </div>   
-                        </div>                    
-                        <input type="submit" class="button blue_bg" id="search_again" value="Search Again" />
-                            <label for="search_again" class="screen-reader-text">
-                            Search Again
+        <h1 class="page-title">Search Results: <?php echo esc_attr(get_search_query()); ?></h1>
+        <form method="GET" action="<?php echo home_url( '/' ); ?>" role="search" aria-label="Utility Bar Search">
+            <div class="row">
+                <div class="large-7 columns">
+                    <div class="row collapse">
+                        <div class="small-10 columns">
+                            <label for="s" class="screen-reader-text">
+                                Search This Website
                             </label>
-                    </fieldset>
-               </form>        
-
-        <?php }
-            } catch (KSAS_GoogleSearchException $e) {
-                $docTitle = "Search Temporarily Unavailable";
+                            <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search this site" aria-label="Search This Website"/>
+                        </div>
+                        <div class="small-2 columns">
+                            <button class="button postfix">
+                            <span class="fa fa-search" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+        <article <?php post_class(''); ?> itemscope itemtype="http://schema.org/BlogPosting" aria-labelledby="post-<?php the_ID(); ?>">
+            <header class="article-header" aria-label="<?php the_title();?>">
+                <h3 class="entry-title single-title search-result" itemprop="headline">
+                    <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                </h3>
+            </header> <!-- end article header -->
+                
+            <div class="entry-content" itemprop="articleBody">
+                <?php $content = get_the_content();
+                $trimmed_content = wp_trim_words( $content, 60, '[...]' ); ?>
+                <p><?php echo $trimmed_content; ?></p>
+            </div> <!-- end article section -->
+                
+        </article> <!-- end article -->
+        <hr>
+        <?php endwhile; ?>
+        <?php
+            global $wp_query;
+            $big = 999999999; // need an unlikely integer
+            echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages
+            ) );
         ?>
-        <div class="section">
-            <p>We're sorry, the search engine is temporarily unavailable. Please try your search again later.</p>
-        </div>
-        <?php } ?>
-
+        <?php else : ?>
+        
+        <header class="article-header">
+            <h3><?php _e('Sorry, No Results.', 'jointswp');?></h3>
+        </header>
+        
+        <section class="entry-content">
+            <p><?php _e('Try your search again.', 'jointswp');?></p>
+        </section>
+        
+        <?php endif; ?>
     </main>
 </div>
-
-<?php get_footer(); ?>  
+<?php get_footer(); ?>
