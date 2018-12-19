028f8bc93c60891ecc3b89b22a5892406ae7ef03
ksascomm <ksasweb@jhu.edu>
2018-12-19 09:57:47 -0500

    search-mobile fixes

--- parts/search-mobile.php
+++ parts/search-mobile.php
@@ -1,19 +1,18 @@
-<div class="search-bar small-12 columns">
+<div class="search-bar small-12 columns" aria-hidden="true">
 	<div class="row">
 		<div class="small-12 columns">
 		<?php $theme_option = flagship_sub_get_global_options(); 
 				$collection_name = $theme_option['flagship_sub_search_collection'];
 		?>
-			<form method="GET" action="<?php echo site_url('/search'); ?>" role="search">
-				<input type="hidden" name="site" value="<?php echo $collection_name; ?>" aria-label="<?php echo $collection_name; ?>"/>
+			<form method="GET" action="<?php echo home_url( '/' ); ?>">
 				<button type="submit" aria-label="submit"/>
 					<span class="fa fa-search"></span>
 				</button>
 				<label for="search-mobile" class="screen-reader-text">
 					Search This Website
 				</label>
-				<input type="text" id="search-mobile" name="q" placeholder="Search this site" aria-label="search"/>	
+				<input type="text" id="search-mobile" value="<?php echo get_search_query(); ?>" name="s" placeholder="Search this site"/>
 			</form>
 		</div>
 	</div>	
-</div>	<!-- End #search-bar	 -->
+</div>	<!-- End #search-bar-->
